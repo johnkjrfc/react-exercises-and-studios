26299639235b58d24eed6379cce43044d8320bd5
johnkjrfc <johnkjrfc@gmail.com>
2023-12-14 13:18:57 -0600

    1st commit

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let newBooks = "shrug";
+   let book1 = "https://images-na.ssl-images-amazon.com/images/I/91E3pwI7MbL._AC_UL254_SR254,254_.jpg";
+   let book2 = "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T1/images/I/71WebVcbSxL._AC_UL254_SR254,254_.jpg";
+   let book3 = "https://images-na.ssl-images-amazon.com/images/I/61nl0iUliZL._AC_UL254_SR254,254_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Art of Mixology" />
+         <img src={book2} alt="Letters from a Stoic" />
+         <img src={book3} alt="What Money Can't Buy" />
       </div>      
    );
 }
