a2a8f0103b9a79fc8b58012b200e495b30dc2831
johnkjrfc <johnkjrfc@gmail.com>
2023-12-18 10:10:13 -0600

    Booklist comp complete

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -3,7 +3,8 @@ export default function BookList() {
    let book1 = "https://images-na.ssl-images-amazon.com/images/I/91E3pwI7MbL._AC_UL254_SR254,254_.jpg";
    let book2 = "https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T1/images/I/71WebVcbSxL._AC_UL254_SR254,254_.jpg";
    let book3 = "https://images-na.ssl-images-amazon.com/images/I/61nl0iUliZL._AC_UL254_SR254,254_.jpg";
-
+   let pageTitle = "Books"
+   
    return (
       <div>
          <h3>{pageTitle}</h3>
